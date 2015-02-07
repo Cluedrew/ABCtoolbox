4b26df84acef066da9590447955580689ca4730b
Cluedrew <cluedrew@siductionbox>
2015-02-07 09:21:45 -0500

    Added some files to try and get the linking between outside code and the outside world going.

---
+++ ABCdefine.hpp
@@ -0,0 +1,12 @@
+#ifndef ABC_DEFINE_H
+#define ABC_DEFINE_H 1.0
+
+// Preprocesser defines
+
+
+
+#define ABC_CODEPATH ../private
+// Defines a path from the include directory to the code directory.
+// I could make it a macro function to give each tool a directory.
+
+#endif//ABC_DEFINE_H
---
+++ include/ABChello.hpp
@@ -0,0 +1,26 @@
+#ifndef ABC_HELLO_H
+#define ABC_HELLO_H 1.0
+
+/* Hello,
+ * 
+ * This header includes just enough to make sure that the tool box is working.
+ * A call to abc::HelloWorld will print to output to establish that the
+ * tool box is getting
+ */
+
+//#include "../private/hello-world.h"
+
+
+
+namespace abc
+{
+void HelloWorld (void);
+/* The small "hello world" function. It prints hello world to stdout.
+ * Prams.: void
+ * Effect: Prints a message to the standard output.
+ * Return: void
+ * Except: no-throw
+ */
+}
+
+#endif//ABC_HELLO_H
