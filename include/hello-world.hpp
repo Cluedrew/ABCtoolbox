bec9bd32b12cbe2ebed4b5e7ee4165798a90b2f4
Cluedrew <cluedrew@siductionbox>
2015-04-24 06:27:00 -0400

    Started testing the Makefile. The public header creation rule works. The linking rule for final objects does not, the error appears to be in the filter, working on a solution. Also updated the file structure diagram in the DevManual.txt.

---
+++ include/hello-world.hpp
@@ -0,0 +1,19 @@
+#ifndef ABCDEF_HELLO_WORLD_H
+#define ABCDEF_HELLO_WORLD_H 1.0
+
+// Connection testing tool.
+
+
+
+namespace abc
+{
+void HelloWorld (void);
+/* The small "hello world" function. It prints hello world to stdout.
+ * Prams.: void
+ * Effect: Prints a message to the standard output.
+ * Return: void
+ * Except: no-throw
+ */
+}
+
+#endif//ABCDEF_HELLO_WORLD_H
