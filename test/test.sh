43c507eb1e784569a8e1946b377e9fdd0d99cf7b
Cluedrew <cluedrew@siductionbox>
2015-06-05 20:07:05 -0400

    Did a bit more work on the test harness.

--- test/test.sh
+++ test/test.sh
@@ -2,10 +2,30 @@
 
 # Testing harness for the ABC tool box.
 
+# Add usage, help, checks and so on.
+# echo $0 [program] [test]
+
+# Test files:
+# *.tst : Test specification.
+# *.in  : Test input, auto tests only.
+# *.out : Expected output, auto tests only, empty if ommited.
+# *.pr  : Prompt file, prompts user, manual tests only.
+# *.res : Results file, system generated.
+
+# Exit Codes:
+# 0 - Success
+# 1 - File Read Error (Missing, Bad Permitions, Incorrect Formating)
+# ? - ???
+
+##############################################################################
+############################### IMPLEMENTATION ###############################
+
 echo "Not yet implemented"
 exit 0
 
-# echo $0 [program] [test]
+# Set the present working directory.
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# Stole that line.
 
 # Creating all the file names.
 prefix=$1/$2
@@ -15,15 +35,23 @@ outfile=${prefix}.out
 prfile =${prefix}.pr
 resfile=${prefix}.res
 
-test=grep -E -e "Test:" -f ${prefix}.tst
-code=grep -E -e "Exit Code:" -f ${prefix}.tst
-call=grep -E -e "Call:" -f ${prefix}.tst
+# Is it an "auto" or "manual" test?
+test=grep -E -e "Test Type:" -f ${tstfile}
+# What is the expected exit code?
+code=grep -E -e "Exit Code:" -f ${tstfile}
+# Arguments to pass to the call. (Whole call?)
+call=grep -E -e "Call Args:" -f ${tstfile}
 
 if [ ${test} = auto ]; then
-  # Create a temp directory
+  # Create a temp file
   # Redirect the output to it
 elif [ ${test} = manual ]; then
   # Prompt the user.
+  # If postive, run the program.
+  # Ask for pass/fail.
+  # If fail, ask for feedback.
 else
   # Invalid test peramiter.
+  echo "Test was not specified 'auto' or 'manual'."
+  exit 1
 fi
